846c04825f3fac759eb7b32140d7fb2c6eff8dc1
Ted Whalen <tewhalen@lumpyspace.local>
2017-11-22 12:59:29 -0600

    change thin separator to slash

--- powerline_shell/__init__.py
+++ powerline_shell/__init__.py
@@ -52,7 +52,7 @@ class Powerline(object):
             'lock': u'\uE0A2',
             'network': 'SSH',
             'separator': u'\uE0B0',
-            'separator_thin': u'\uE0B1'
+            'separator_thin': "/" #u'\uE0B1'
         },
         'flat': {
             'lock': u'\uE0A2',
